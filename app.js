b9d68b4b0fd3112b1855a2f702e63726503f0416
Laisture <jqlle_kqlle@hotmail.com>
2021-05-04 18:53:45 +0900

    inital commit

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+//var path = require('path');
+//app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+
+module.exports = app;
